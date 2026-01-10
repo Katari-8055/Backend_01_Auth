e06258e640f06ba7eb09e2b404383f21fff21cba
Katari <kataricoder@gmail.com>
2026-01-10 22:49:29 +0530

    Changing all Things from scratch

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
-import express, { type Application } from "express";
+import express from "express";
+// import cors from "cors";
+import helmet from "helmet";
+// import morgan from "morgan";
+import { errorMiddleware } from "./middlewares/error.middleware";
 
-const app: Application = express();
 
-// middlewares
+
+const app = express();
+
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(cors());
+app.use(helmet());
+// app.use(morgan("dev"));
 
-// health check
-app.get("/health", (_req, res) => {
-  res.status(200).json({ status: "OK" });
-});
+// Define your routes here
+
+// global error handler
+app.use(errorMiddleware);
 
 export default app;
--- src/server.ts
+++ src/server.ts
@@ -1,24 +1,13 @@
 import dotenv from "dotenv";
-import app from "./app.js";
-import connectDB from "../config/db";
 dotenv.config();
 
+import app from "./app";
+import connectDB from "./config/db";
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 
-const startServer = async (): Promise<void> => {
-  try {
-    await connectDB();
-
-    app.listen(PORT, () => {
-      console.log(`🚀 Server running on port ${PORT}`);
-    });
-  } catch (error) {
-    console.error("❌ Server failed to start:", error);
-    process.exit(1);
-  }
-};
-
-
-
-startServer();
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`🚀 Server running on port ${PORT}`);
+  });
+});
